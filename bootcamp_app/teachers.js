958d423fc7b77956a8a36853f3dd57f24fb3ab9d
Phil <philippe.teigne@gmail.com>
2024-01-10 00:10:44 -0500

    add query selector for teachers that assisted for a given cohort

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.query('SELECT * FROM pg_catalog.pg_tables', function(err, result) {
+//   console.log(result);
+// });
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teachers.name;
+`)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  });
